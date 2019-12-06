4a03c155790cac51a2272fdf39ff6edbd6362c03
kopchikovich <kopchikovich@yandex.ru>
2019-12-06 14:20:28 +1000

    Make book styles

--- src/components/book.js
+++ src/components/book.js
@@ -5,16 +5,20 @@ const Book = (props) => {
     const book = db[props.bookId];
     const dateOfReading = book.dateOfReading.toDateString();
     const quotes = book.quotes.map((item, i) => {
-        return <li key={i}>{item}</li>
+        return <li key={i}>"{item}"</li>
     });
     return  (
-        <article>
-            <img src={book.img? book.id : 'img/empty.jpg'} alt='book cover'/>
-            <p>{book.title}</p>
-            <p>{book.author}</p>
-            <p>{dateOfReading}</p>
-            <p>{book.mainIdea}</p>
-            <ul>{quotes}</ul>
+        <article className='book'>
+            <img
+                className='book__cover'
+                src={book.img? book.id : 'img/empty.jpg'} 
+                alt='book cover'/>
+            <p className='book__title'>"{book.title}"</p>
+            <p className='book__author'>{book.author}</p>
+            <p className='book__text book__date'>Прочитано: {dateOfReading}</p>
+            <p className='book__text book__text--line'>Главная мысль: {book.mainIdea}</p>
+            <p className='book__text book__text--left'>Цитаты:</p>
+            <ul className='book__text'>{quotes}</ul>
         </article>
     )
 }
