e0168527b580037fe76dc29866d770f812d2689a
kopchikovich <kopchikovich@yandex.ru>
2019-12-08 16:10:07 +1000

    Add login screen render property

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,17 @@ import SearchInput from './search-input'
 import Button from './button'
 
 const Header = (props) => {
-    const searchInput = <SearchInput searchHandler={props.searchHandler} className='header__search-input search-input' />
-    const listButton = <Button onClickHandler={props.openSearchScreen} title='list' className='header__button button' />
+
+    const {screen} = props
+    const searchInput = <SearchInput  className='header__search-input search-input' key='search-inpt' searchHandler={props.searchHandler} />
+    const listButton = <Button className='header__button button' key='list-btn' title='list' onClickHandler={props.openSearchScreen} />
+    const userButton = <Button className='header__button button button--percon-icon' key='user-btn' onClickHandler={props.openUserScreen} />
+
     return (
         <header className='app__header header'>
-            {props.screen === 'search' ? searchInput : listButton}
-            <Button className='header__button button button--percon-icon' onClickHandler={props.openUserScreen} />
+
+            {screen === 'login'? null : screen === 'search'? [searchInput, userButton] : [listButton, userButton]}
+
         </header>
     )
 }
