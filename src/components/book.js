77a02e96de9c02c8344d9a65d0c0c758ac6c1876
kopchikovich <kopchikovich@yandex.ru>
2019-12-10 14:20:05 +1000

    Add controls to book screen

--- src/components/book.js
+++ src/components/book.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Button from './button'
 import {db, sortedBooks} from '../data'
 
 const Book = (props) => {
@@ -8,19 +9,25 @@ const Book = (props) => {
     const quotes = book.quotes.map((item, i) => {return <li key={i}>"{item}"</li>});
 
     return  (
-        <article className='book'>
-            <img
-                className='book__cover'
-                src={book.img? book.img : 'img/empty.jpg'} 
-                alt='book cover'
-            />
-            <p className='book__title'>"{book.title}"</p>
-            <p className='book__author'>{book.author}</p>
-            <p className='book__text book__date'>Прочитано: {dateOfReading}</p>
-            <p className='book__text book__text--line'>Главная мысль: {book.mainIdea}</p>
-            <p className='book__text book__text--left'>Цитаты:</p>
-            <ul className='book__text'>{quotes}</ul>
-        </article>
+        <section>
+            <article className='book'>
+                <img
+                    className='book__cover'
+                    src={book.img? book.img : 'img/empty.jpg'} 
+                    alt='book cover'
+                />
+                <p className='book__title'>"{book.title}"</p>
+                <p className='book__author'>{book.author}</p>
+                <p className='book__text book__date'>Прочитано: {dateOfReading}</p>
+                <p className='book__text book__text--line'>Главная мысль: {book.mainIdea}</p>
+                <p className='book__text book__text--left'>Цитаты:</p>
+                <ul className='book__text'>{quotes}</ul>
+            </article>
+            <div className='controls'>
+                <Button className='controls__button' title='<' onclickHandler={null} />
+                <Button className='controls__button' title='>' onclickHandler={null} />
+            </div>
+        </section>
     )
 }
 
