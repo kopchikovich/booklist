17d544a65c61aa1e375c26fef0dbeaf06643f441
kopchikovich <kopchikovich@yandex.ru>
2019-12-12 16:36:16 +1000

    Add icon to edit button

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,8 @@ const Header = (props) => {
     const {screen} = props
     const searchInput = <SearchInput  className='input header__search-input' key='search-inpt' searchHandler={props.searchHandler} />
     const listButton = <Button className='header__button header__button--list button' key='list-btn' title='booklist' onClickHandler={props.openBookListScreen} />
-    const editButton = screen === 'book' && <Button className='controls__button controls__button--edit' key='edit-btn' title='edit' onClickHandler={null} />
+    const editButton = screen === 'book' && <Button className='header__button header__button--edit button' key='edit-btn'
+    title={<span className='button--edit__icon'/>} onClickHandler={null} />
     const addButton = <Button className='header__button button button--add' key='add-btn' title='+' onClickHandler={props.openAddBookScreen} />
     const userButton = <Button className='header__button button button--percon-icon' key='user-btn' onClickHandler={props.openUserScreen} />
 
