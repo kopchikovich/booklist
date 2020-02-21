171d155c0174181debfde33f1363cd94f858160d
kopchikovich <kopchikovich@yandex.ru>
2020-02-22 09:09:10 +1000

    Refactoring message api

--- src/index.js
+++ src/index.js
@@ -3,16 +3,27 @@ import {render} from 'react-dom'
 import App from './components/app'
 import Db from './db'
 
+// make controller for app
 document.controller = {}
-// message api
+
+// add message api to controller
 document.controller.renderMessage = (text, color) => {
+
+    const makeMessage = (text, color) => {
+        const message = document.createElement('div');
+        const renderColor = color === 'green'? '#0a0' : '#a00';
+        message.className = 'message';
+        message.innerHTML = text;
+        message.style.color = renderColor;
+        return message;
+    }
 
     const TOP_POSITION = 0;
     const TIMEOUT = 3000;
     const container = document.getElementById('message');
     const message = makeMessage(text, color);
-
     container.appendChild(message);
+
     // for animation
     setTimeout(() => {
         message.style.top = TOP_POSITION;
@@ -21,19 +32,6 @@ document.controller.renderMessage = (text, color) => {
         container.removeChild(message);
     }, TIMEOUT);
 }
-
-const makeMessage = (text, color) => {
-
-    const message = document.createElement('div');
-    const renderColor = color === 'green'? '#0a0' : '#a00';
-
-    message.className = 'message';
-    message.innerHTML = text;
-    message.style.color = renderColor;
-    
-    return message;
-}
-
 
 // make or connect to db
 const booksDb = new Db('books')
